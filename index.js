51d33e354c46a95f6ed5f45a555903c6fd56d6ff
Daniel <danieljameskay@gmail.com>
2018-05-16 19:25:12 +0100

    Further changes.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
+
+const port = process.env.PORT || 1337;
+
 io.on('connection', function(client){
-  client.on('event', function(data){
+  client.on('current_loc', function(data){
     console.log(data)
-    
+
   });
   client.on('disconnect', function(){});
 });
 
-server.listen(1234);
+server.listen(port);
