3aabff94b7a7e85b31fa23e6847ae465ceb54aed
Dan Kay <danieljameskay@gmail.com>
2018-05-18 09:06:39 +0100

    Added node-rdkafka to sent events to Kafka.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
-
+const kafka = require("node-rdkafka");
+require('dotenv').config();
 
 const port = process.env.PORT || 1337;
 
-io.on('connection', function(client){
-  client.on('current_loc', function(data){
-    console.log(data)
+const producer = new kafka.Producer({
+  'metadata.broker.list' : process.env.KAFKA_BROKER_URL,
+  'dr_cb': true
+})
 
+producer.connect(err => { console.log(err) });
+
+io.on('connection', client => {
+  producer.on('ready', () => { 
+    client.on('current_loc', data => {
+      try {
+        producer.produce('car_loc', null, new Buffer(data), null, Date.now);
+      } catch(e) {
+        console.error('A problem occurred when sending our message.');
+        console.error(e);
+      };
+    });
+    client.on('disconnect', () => {console.log("A driver disconnected,")});
   });
-  client.on('disconnect', function(){});
+  producer.on('event.error', function(err) {
+    console.error('Error from producer');
+    console.error(err);
+  })
 });
 
 server.listen(port);
+console.log(`Listening on port ${port}.`);
